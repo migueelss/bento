4ab2ac8a6cd8b693451afa05d0254bc5bbe49a46
migueelss <bms.santos@outlook.pt>
2024-08-06 20:14:16 +0100

    Frameless window with custom bar

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
-    icon: './images/bento/bento.ico'
+    icon: './images/bento/bento.ico',
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#2c2c2c',
+      symbolColor: 'white',
+      height: 38
+    }
   })
   win.setMenuBarVisibility(false)
 
